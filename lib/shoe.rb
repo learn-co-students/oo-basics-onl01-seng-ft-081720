d6a2ed1a43544bc2251f157bb92846bd543894e8
Newland-A <ajn252@gmail.com>
2020-08-25 10:43:13 -0400

    Done.

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,19 @@
 # Make your shoe class here!
+class Shoe
+  #automatically sets methods for color, size, material and condition
+  attr_accessor :color, :size, :material, :condition
+  attr_reader :brand
+#initializes the method for a variable
+  def initialize(brand)
+    #assigns the instance method to a variable
+    @brand = brand
+  end
+ #defines a method for cobble
+  def cobble
+    #puts out a messsage
+    puts "Your shoe is as good as new!"
+    #calls on the method condition and sets it to new 
+    @condition = "new"
+  end
+  
+end
